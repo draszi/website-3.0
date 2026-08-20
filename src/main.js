16b3e06fee9e37ff9752aa61d11b07d26d1ed8a8
Arbi <arbidaci@gmail.com>
2026-08-20 16:20:20 -0400

    fixed resume link

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import './style.css'
 import './blob.js'
-import './Extra/exp.js'
-import './Extra/exp.css'
-import './Extra/details.js'
+// import './Extra/exp.js'
+// import './Extra/exp.css'
+// import './Extra/details.js'
 
 import resumePDF from './assets/ArbiDaciResume.pdf';
 document.getElementById('resume-link').href = resumePDF;
